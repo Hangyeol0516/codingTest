4086607c41c8c5f8a112b1c8d263bb1cdc033bf0
Hangyeol0516 <127859370+Hangyeol0516@users.noreply.github.com>
2023-03-19 15:35:06 +0900

    [Bronze V] Title: 나머지, Time: 212 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner( System.in );
+		
+		int A,B,C;
+		
+		A = scan.nextInt();
+		B = scan.nextInt();
+		C = scan.nextInt();
+		
+		System.out.println((A+B)%C);
+		System.out.println(((A%C) + (B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C) * (B%C))%C);
+		
+		
+	}
+	
+}
