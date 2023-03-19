1eaa134aa976bc50d80533dc853bdfd83f7f6a17
Hangyeol0516 <127859370+Hangyeol0516@users.noreply.github.com>
2023-03-19 15:30:48 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner( System.in );
+		
+		String a;
+		
+		a = scan.next();
+		
+		System.out.println(a + "??!");
+		
+		
+	}
+	
+}
