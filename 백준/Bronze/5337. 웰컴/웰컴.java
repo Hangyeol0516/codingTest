8cf5747a527751774af55596ba7b03674fa33261
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-03-26 16:11:36 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+				
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+		
+		
+			
+	}
+	
+}
