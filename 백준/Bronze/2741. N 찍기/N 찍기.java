584c70051d109aea2f162051bd8ea3554c8f4fe9
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-03-26 15:03:09 +0900

    [Bronze V] Title: N 찍기, Time: 1096 ms, Memory: 33088 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+				
+		Scanner scan = new Scanner( System.in );
+		
+		int n;
+		
+		n = scan.nextInt();
+		
+		for (int i=1;i<=n;i++) {
+			
+			System.out.println(i);
+		}
+	 
+	}
+	
+}
