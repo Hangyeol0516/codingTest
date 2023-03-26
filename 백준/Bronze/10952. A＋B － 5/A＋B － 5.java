da7fb64c26191693b461f5ed9f4212e374a491f2
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-03-26 14:47:13 +0900

    [Bronze V] Title: A+B - 5, Time: 224 ms, Memory: 17908 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+				
+		Scanner scan = new Scanner( System.in );
+		
+		int a,b;
+		
+		while (true) {
+			
+			a = scan.nextInt();
+			b = scan.nextInt();
+			
+			if(a!=0&&b!=0)
+				System.out.println(a+b);
+			else break;
+		}
+	 
+	}
+	
+}
+
