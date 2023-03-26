fb8af479bea2fa1c80d5244e7fea8541017ae5be
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-03-26 15:30:46 +0900

    [Bronze V] Title: A+B - 2, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+				
+		Scanner scan = new Scanner( System.in );
+		
+		int a,b;
+		
+		a = scan.nextInt();
+		b = scan.nextInt();
+			
+		System.out.println(a+b);
+	 
+	}
+	
+}
