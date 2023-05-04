d553955314c650937eeb86c17a051f84633d559a
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-05-04 22:09:41 +0900

    [Bronze V] Title: 아스키 코드, Time: 212 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+				
+		Scanner scan = new Scanner( System.in );
+		
+		String a = scan.next();
+		
+		int ascii = a.charAt(0);
+		
+		System.out.println(ascii);
+		
+	}
+}
