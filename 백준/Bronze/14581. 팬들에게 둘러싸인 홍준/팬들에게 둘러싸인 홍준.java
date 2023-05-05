4a56f9cbd16e0bf9e0364ad84bafe695779ffc90
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-05-05 17:30:07 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 248 ms, Memory: 18216 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+				
+		Scanner scan = new Scanner( System.in );
+		
+		String hongjun = scan.next();
+		
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan:"+":"+hongjun+":"+":fan:");
+		System.out.println(":fan::fan::fan:");
+        
+	}
+}
