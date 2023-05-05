d429ae212ae5bdc2c3ab7893dfaec6ae3a427461
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-05-05 17:34:10 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 204 ms, Memory: 17100 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+				
+		Scanner scan = new Scanner( System.in );
+		
+		int N = scan.nextInt();
+		int K = scan.nextInt();
+		
+		for (int i=1; i<=N; i++) {
+			int A = scan.nextInt();
+			int B = scan.nextInt();
+		}
+		System.out.println("비와이");
+		
+	}
+}
