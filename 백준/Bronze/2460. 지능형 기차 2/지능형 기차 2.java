4a8eac0cb67227305fce81e914e2c089e43d7148
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-05-06 18:00:42 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 216 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+				
+		Scanner scan = new Scanner( System.in );
+		
+		int index = 0;
+		int result = 0;
+		
+		for (int i=1; i<=10; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+
+			index = index + b - a;
+			if (index > result ) {
+				result = index;
+			}
+		}
+		System.out.println(result);	
+	}
+}
