3473a58384920afc018b92aab2c022a3b38dde8c
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-05-11 18:56:25 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 204 ms, Memory: 17540 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int X = scan.nextInt();
+		
+		System.out.println(X*X*X);
+		
+	}
+
+}
