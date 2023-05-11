510f1ae23cccdaaab89f6897d2f1146d88ddc610
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-05-11 18:41:24 +0900

    [Bronze V] Title: 연세대학교, Time: 196 ms, Memory: 17296 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		
+		if (N==0) {
+			System.out.println("YONSEI");
+		} else if (N==1) {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+
+}
