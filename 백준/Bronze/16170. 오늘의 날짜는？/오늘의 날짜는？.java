036299cb69062552fb2dcfa873a2a1dcfc3b6d87
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-05-14 14:25:40 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 112 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("2023");
+        System.out.println("05");
+        System.out.println("14");
+
+    }
+}
