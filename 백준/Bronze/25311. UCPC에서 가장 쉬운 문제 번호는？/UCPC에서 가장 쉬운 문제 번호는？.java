6f6f0b4ac8a25586e2e4099c7602505d676f6a6b
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-06-17 13:13:53 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Scanner scan = new Scanner(System.in);
+        
+        int y = scan.nextInt();
+        
+        System.out.println("A");
+    }
+}
