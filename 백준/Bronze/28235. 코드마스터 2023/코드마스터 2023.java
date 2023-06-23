58a0ad249e601ea9d407b3ed70224f2d366eea8c
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-06-24 01:09:28 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 196 ms, Memory: 17416 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String str = scan.next();
+		
+		if (str.equals("SONGDO")) {
+			System.out.println("HIGHSCHOOL");
+		}
+		if (str.equals("CODE")) {
+			System.out.println("MASTER");
+		}
+		if (str.equals("2023")) {
+			System.out.println("0611");
+		}
+		if (str.equals("ALGORITHM")) {
+			System.out.println("CONTEST");
+		}
+		
+	}
+
+}
