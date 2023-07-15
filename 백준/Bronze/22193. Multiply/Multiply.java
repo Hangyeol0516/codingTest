b1353ca3987f1ba799205460cdbd157a39d2362f
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-07-15 14:32:04 +0900

    [Bronze V] Title: Multiply, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		int M = scan.nextInt();
+		
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		System.out.println(A*B);
+        
+	}
+}
